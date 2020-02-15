32cac3719ab5d0eef62ae90f1952b94b4202f291
Jaeyoung Ban <jban0811@gmail.com>
2020-02-15 16:46:31 +1300

    cleaning up

--- HundredScoreKeeper/components/ScoreScreen/ScoreScreen.js
+++ HundredScoreKeeper/components/ScoreScreen/ScoreScreen.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import styles from './ScoreScreen.style' ;
 
 export default class ScoreScreen extends Component {
   render() {
       return(
         <View style={styles.container}>
-            <Text>Score Keeper</Text>
+            <Text>Hundred Score Keeper</Text>
             <Button
                 title = "Info Screen"
                 onPress = {() => this.props.navigation.navigate("InfoScreen")}
             />
+            <Text>Yes</Text>
         </View>
       );
   }
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-});
